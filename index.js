1439e922c4415b1d736aca7b3e35936b5e4e6758
Martin <martin.hovre@opencubes.io>
2017-09-25 16:56:14 +0200

    Added support for multiple persons to say welp and track it

--- index.js
+++ index.js
@@ -8,20 +8,24 @@ const writeFile = promisify(fs.writeFile)
 
 const client = new eris(process.env["welp_tracker_token"])
 
-let welp = 0
+let welps = {
+    // userid: times
+}
 
-if (fs.existsSync(resolve(".", "counter.txt"))) readFile(resolve(".", "counter.txt"), "utf8").then(countS => {
-    const count = +countS
-    welp += count
+if (fs.existsSync(resolve(".", "welp.json"))) readFile(resolve(".", "welp.json"), "utf8").then(JSON.parse).then(welp => {
+    Object.getOwnPropertyNames(welp).forEach(id => {
+        if (id in welps) welps[id] += welp[id]
+        else welps[id] = welp[id]
+    })
 })
 
 client.on("messageCreate", msg => {
-    if (msg.content === "welp" && msg.author.id === "234676929263828993") {
-        welp++
-        writeFile(resolve('.', 'counter.txt'), ""+welp)
+    if (msg.content === "welp" && msg.author.id in welps) {
+        welps[msg.author.id]++
+        writeFile(resolve('.', 'welp.json'), JSON.stringify(welps), "utf-8")
     }
     if (msg.content === "!welp") {
-        client.createMessage(msg.channel.id, "<@234676929263828993> has said \"welp\" " + welp + " time" + (welp !== 1 ? 's' : ''))
+        client.createMessage(msg.channel.id, msg.author.mention + " has said \"welp\" " + welp + " time" + (welp !== 1 ? 's' : ''))
     }
 })
 
